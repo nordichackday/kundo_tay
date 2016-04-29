d983eb4730392a7e22048af3d2e8d28d45008be3
Stefan Berggren <nsg@nsg.cc>
2016-04-29 11:27:51 +0200

    /ask POST endpoint

--- kundo_webapp/app/app.py
+++ kundo_webapp/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template
 from flask.ext.cors import CORS, cross_origin
 from datetime import datetime
 import urllib2, json, time
+import subprocess
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +11,12 @@ CORS(app)
 def index():
     return render_template("index.html");
 
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    return subprocess.Popen("/app/ask hello",
+            shell=True,
+            stdout=subprocess.PIPE).stdout.read()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
